6232cbd5d5f96526f3bc6ad6ec9f691a97d57960
Xiaochun Shen <snowdonshen@gmail.com>
2011-08-08 21:52:53 +0800

    init

---
+++ Just/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Just
+//
+//  Created by  Chuns on 11-8-8.
+//  Copyright (c) 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) IBOutlet UIWindow *window;
+
+@end
